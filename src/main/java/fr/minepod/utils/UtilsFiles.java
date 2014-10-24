0a6a5a5fbecfe6e981937b6843724b4a6b1e9d71
DarkShimy00 <darkshimy00@gmail.com>
2014-10-24 14:09:55 +0200

    Version 1.4.2.0 | API Breaking
    * Formatting and cleanup

--- src/main/java/fr/minepod/utils/UtilsFiles.java
+++ src/main/java/fr/minepod/utils/UtilsFiles.java
@@ -23,189 +23,191 @@ import net.lingala.zip4j.util.Zip4jConstants;
 import org.bouncycastle.util.encoders.Hex;
 import org.mozilla.universalchardet.UniversalDetector;
 
-public class UtilsFiles {	
-	public static String readFile(String path) throws IOException {
-		return readFile(new File(path));
-	}
+public class UtilsFiles {
+  public static String readFile(String path) throws IOException {
+    return readFile(new File(path));
+  }
 
-	public static String readFile(File file) throws IOException {
-		return readFileWithEncoding(new InputStreamReader(new FileInputStream(file)));
-	}
+  public static String readFile(File file) throws IOException {
+    return readFileWithEncoding(new InputStreamReader(new FileInputStream(file)));
+  }
 
-	public static String readFileUtf8(File file) throws IOException {
-		return readFileWithEncoding(new InputStreamReader(new FileInputStream(file), "UTF-8"));
-	}
+  public static String readFileUtf8(File file) throws IOException {
+    return readFileWithEncoding(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+  }
 
-	public static String readFileWithEncoding(Reader reader) throws IOException {
-		BufferedReader br = new BufferedReader(reader);
-		try {
-			StringBuilder sb = new StringBuilder();
-			String line = br.readLine();
+  public static String readFileWithEncoding(Reader reader) throws IOException {
+    BufferedReader br = new BufferedReader(reader);
+    try {
+      StringBuilder sb = new StringBuilder();
+      String line = br.readLine();
 
-			boolean first = true;
-			while (line != null) {
-				if(first) {
-					first = false;
-				} else {
-					sb.append("\n");
-				}
-				sb.append(line);
-				line = br.readLine();
-			}
-			return sb.toString();
-		} finally {
-			br.close();
-		}
-	}
+      boolean first = true;
+      while (line != null) {
+        if (first) {
+          first = false;
+        } else {
+          sb.append("\n");
+        }
+        sb.append(line);
+        line = br.readLine();
+      }
+      return sb.toString();
+    } finally {
+      br.close();
+    }
+  }
 
-	public static void writeFile(String path, String stringToWrite) throws IOException {
-		writeFile(new File(path), stringToWrite);
-	}
+  public static void writeFile(String path, String stringToWrite) throws IOException {
+    writeFile(new File(path), stringToWrite);
+  }
 
-	public static void writeFile(File file, String stringToWrite) throws IOException {
-		if(!file.exists()) {
-			file.createNewFile();
-		}
+  public static void writeFile(File file, String stringToWrite) throws IOException {
+    if (!file.exists()) {
+      file.createNewFile();
+    }
 
-		writeFileWithEncoding(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"), stringToWrite);
-	}
+    writeFileWithEncoding(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"),
+        stringToWrite);
+  }
 
-	public static void writeFileWithEncoding(Writer writer, String stringToWrite) throws IOException {
-		BufferedWriter bw = new BufferedWriter(writer);
-		bw.write(stringToWrite);
-		bw.close();
-	}
+  public static void writeFileWithEncoding(Writer writer, String stringToWrite) throws IOException {
+    BufferedWriter bw = new BufferedWriter(writer);
+    bw.write(stringToWrite);
+    bw.close();
+  }
 
-	public static String getFileEncoding(File file) throws IOException {
-		byte[] buf = new byte[4096];
-		FileInputStream fileInputStream = new FileInputStream(file);
-		UniversalDetector detector = new UniversalDetector(null);
+  public static String getFileEncoding(File file) throws IOException {
+    byte[] buf = new byte[4096];
+    FileInputStream fileInputStream = new FileInputStream(file);
+    UniversalDetector detector = new UniversalDetector(null);
 
-		int nread;
-		while ((nread = fileInputStream.read(buf)) > 0 && !detector.isDone()) {
-			detector.handleData(buf, 0, nread);
-		}
-		detector.dataEnd();
+    int nread;
+    while ((nread = fileInputStream.read(buf)) > 0 && !detector.isDone()) {
+      detector.handleData(buf, 0, nread);
+    }
+    detector.dataEnd();
 
-		String encoding = detector.getDetectedCharset();
-		detector.reset();
-		fileInputStream.close();
+    String encoding = detector.getDetectedCharset();
+    detector.reset();
+    fileInputStream.close();
 
-		return encoding.toUpperCase();
-	}
+    return encoding.toUpperCase();
+  }
 
-	public static void copyFile(String input, String output) throws IOException {
-		copyFile(new File(input), new File(output));
-	}
+  public static void copyFile(String input, String output) throws IOException {
+    copyFile(new File(input), new File(output));
+  }
 
-	public static void copyFile(File input, File output) throws IOException {
-		if(output.exists()) {
-			output.delete();
-		}
+  public static void copyFile(File input, File output) throws IOException {
+    if (output.exists()) {
+      output.delete();
+    }
 
-		InputStream inputStream = new FileInputStream(input);
-		OutputStream outputStream = new FileOutputStream(output);
+    InputStream inputStream = new FileInputStream(input);
+    OutputStream outputStream = new FileOutputStream(output);
 
-		byte[] buffer = new byte[1024];
-		int length;
-		while((length = inputStream.read(buffer)) > 0){
-			outputStream.write(buffer, 0, length);
-		}
+    byte[] buffer = new byte[1024];
+    int length;
+    while ((length = inputStream.read(buffer)) > 0) {
+      outputStream.write(buffer, 0, length);
+    }
 
-		inputStream.close();
-		outputStream.close();
-	}
+    inputStream.close();
+    outputStream.close();
+  }
 
-	public static void delete(String path) {
-		delete(new File(path));
-	}
+  public static void delete(String path) {
+    delete(new File(path));
+  }
 
-	public static void delete(File file) {		
-		if(file.exists()) {
-			if(file.isDirectory() && file.list().length != 0) {
-				String files[] = file.list();
-				for (String temp : files) {
-					File fileDelete = new File(file, temp);
-					delete(fileDelete);
-				}
-			}
-			file.delete();
-		}
-	}
+  public static void delete(File file) {
+    if (file.exists()) {
+      if (file.isDirectory() && file.list().length != 0) {
+        String files[] = file.list();
+        for (String temp : files) {
+          File fileDelete = new File(file, temp);
+          delete(fileDelete);
+        }
+      }
+      file.delete();
+    }
+  }
 
-	public static void unZip(String input, String dirOutput, String fileOutputName) throws ZipException {
-		delete(new File(dirOutput + fileOutputName));
-		ZipFile zipFile = new ZipFile(input);
-		zipFile.extractAll(dirOutput);
-	}
+  public static void unZip(String input, String dirOutput, String fileOutputName)
+      throws ZipException {
+    delete(new File(dirOutput + fileOutputName));
+    ZipFile zipFile = new ZipFile(input);
+    zipFile.extractAll(dirOutput);
+  }
 
-	public static void zip(String input, String output) throws ZipException {
-		zip(new File(input), new File(output));
-	}
+  public static void zip(String input, String output) throws ZipException {
+    zip(new File(input), new File(output));
+  }
 
-	public static void zip(File input, File output) throws ZipException {
-		File[] inputFiles = new File[]{input};
-		zip(inputFiles, output);
-	}
+  public static void zip(File input, File output) throws ZipException {
+    File[] inputFiles = new File[] {input};
+    zip(inputFiles, output);
+  }
 
-	public static void zip(String[] input, String output) throws ZipException {
-		File[] inputFiles = new File[]{};
+  public static void zip(String[] input, String output) throws ZipException {
+    File[] inputFiles = new File[] {};
 
-		for(int i = 0; i < input.length; i++) {
-			inputFiles[i] = new File(input[i]);
-		}
+    for (int i = 0; i < input.length; i++) {
+      inputFiles[i] = new File(input[i]);
+    }
 
-		zip(inputFiles, new File(output));
-	}
+    zip(inputFiles, new File(output));
+  }
 
-	public static void zip(File[] input, File output) throws ZipException {
-		output.delete();
-		ZipFile zipFile = new ZipFile(output);
-		ZipParameters parameters = new ZipParameters();
-		parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
-		parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
+  public static void zip(File[] input, File output) throws ZipException {
+    output.delete();
+    ZipFile zipFile = new ZipFile(output);
+    ZipParameters parameters = new ZipParameters();
+    parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
+    parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
 
-		for(int i = 0; i < input.length; i++) {
-			if(input[i].isDirectory()) {
-				zipFile.addFolder(input[i].getAbsoluteFile(), parameters);
-			} else {
-				zipFile.addFile(input[i].getAbsoluteFile(), parameters);
-			}
-		}
-	}
+    for (int i = 0; i < input.length; i++) {
+      if (input[i].isDirectory()) {
+        zipFile.addFolder(input[i].getAbsoluteFile(), parameters);
+      } else {
+        zipFile.addFile(input[i].getAbsoluteFile(), parameters);
+      }
+    }
+  }
 
-	public static String md5(String path) throws NoSuchAlgorithmException, IOException {
-		return md5(new File(path));
-	}
+  public static String md5(String path) throws NoSuchAlgorithmException, IOException {
+    return md5(new File(path));
+  }
 
-	public static String md5(File file) throws NoSuchAlgorithmException, IOException {
-		return encodeFile(file, MessageDigest.getInstance("MD5"));
-	}
+  public static String md5(File file) throws NoSuchAlgorithmException, IOException {
+    return encodeFile(file, MessageDigest.getInstance("MD5"));
+  }
 
-	public static String sha256(String path) throws NoSuchAlgorithmException, IOException {
-		return sha256(new File(path));
-	}
+  public static String sha256(String path) throws NoSuchAlgorithmException, IOException {
+    return sha256(new File(path));
+  }
 
-	public static String sha256(File file) throws NoSuchAlgorithmException, IOException {
-		return encodeFile(file, MessageDigest.getInstance("SHA-256"));
-	}
+  public static String sha256(File file) throws NoSuchAlgorithmException, IOException {
+    return encodeFile(file, MessageDigest.getInstance("SHA-256"));
+  }
 
-	public static String encodeFile(File file, MessageDigest md) throws IOException {
-		if ((file.exists()) && (file.length() > 0L)) {
-			FileInputStream fis = new FileInputStream(file);
-			byte[] dataBytes = new byte[1024];
-			int nread = 0;
+  public static String encodeFile(File file, MessageDigest md) throws IOException {
+    if ((file.exists()) && (file.length() > 0L)) {
+      FileInputStream fis = new FileInputStream(file);
+      byte[] dataBytes = new byte[1024];
+      int nread = 0;
 
-			while ((nread = fis.read(dataBytes)) != -1) {
-				md.update(dataBytes, 0, nread);
-			}
+      while ((nread = fis.read(dataBytes)) != -1) {
+        md.update(dataBytes, 0, nread);
+      }
 
-			byte[] mdbytes = md.digest();
-			fis.close();
+      byte[] mdbytes = md.digest();
+      fis.close();
 
-			return new String(Hex.encode(mdbytes));
-		}
+      return new String(Hex.encode(mdbytes));
+    }
 
-		return null;
-	}
+    return null;
+  }
 }
--- src/main/java/fr/minepod/utils/UtilsLogger.java
+++ src/main/java/fr/minepod/utils/UtilsLogger.java
@@ -7,35 +7,43 @@ import java.util.logging.FileHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
-class PrettyFormatter extends Formatter {   
-	private static final MessageFormat messageFormat = new MessageFormat("{0}[{1}|{2}|{3,date,h:mm:ss}]: {4} \n");
+class PrettyFormatter extends Formatter {
+  private static final MessageFormat messageFormat = new MessageFormat(
+      "{0}[{1}|{2}|{3,date,h:mm:ss}]: {4} \n");
 
-	public PrettyFormatter() {
-		super();
-	}
+  public PrettyFormatter() {
+    super();
+  }
 
-	@Override public String format(LogRecord record) {
-		Object[] arguments = new Object[6];
-		arguments[0] = record.getLoggerName();
-		arguments[1] = record.getLevel();
-		arguments[2] = Thread.currentThread().getName();
-		arguments[3] = new Date(record.getMillis());
-		arguments[4] = record.getMessage();
-		return messageFormat.format(arguments);
-	}	
+  @Override
+  public String format(LogRecord record) {
+    Object[] arguments = new Object[6];
+
+    arguments[0] = record.getLoggerName();
+    arguments[1] = record.getLevel();
+    arguments[2] = Thread.currentThread().getName();
+    arguments[3] = new Date(record.getMillis());
+    arguments[4] = record.getMessage();
+
+    return messageFormat.format(arguments);
+  }
 }
 
+
 public class UtilsLogger {
-	public java.util.logging.Logger SetLogger(String LogFile) throws SecurityException, IOException {
-		java.util.logging.Logger logger = java.util.logging.Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
-		logger.setLevel(Level.INFO);
-		if(LogFile != null) {
-			FileHandler file = new FileHandler(LogFile);
-			PrettyFormatter formatter = new PrettyFormatter();
-			file.setFormatter(formatter);
-			logger.addHandler(file);
-		}
-		return logger;
-	}
+  public static Logger setLogger(String LogFile) throws SecurityException, IOException {
+    Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    logger.setLevel(Level.INFO);
+
+    if (LogFile != null) {
+      FileHandler file = new FileHandler(LogFile);
+      PrettyFormatter formatter = new PrettyFormatter();
+      file.setFormatter(formatter);
+      logger.addHandler(file);
+    }
+
+    return logger;
+  }
 }
